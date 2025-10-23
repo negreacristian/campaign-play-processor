8e3a27542497e1072373548b8adb579906eafd43
Cristi Negrea <negreacristi22@gmail.com>
2025-10-23 09:58:49 +0300

    fix: campaign structure in frontend

--- frontend/src/types/campaign.ts
+++ frontend/src/types/campaign.ts
@@ -1,8 +1,9 @@
 // define structure of Campaign description
 
 export interface Campaign {
-  _id: string;
+
   campaign_id: string;
   screen_id: string;
+  timestamp: string;
   total_plays: number;
 }
